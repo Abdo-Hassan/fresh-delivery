c925cc7fc7478afeaca8502d872d87c7f5326955
abdo <abdoholmes97@gmail.com>
2022-07-16 21:19:41 +0200

    chore: installed sanity

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -17,7 +17,7 @@ const RestaurantCards = ({
     <TouchableOpacity activeOpacity={0.5} className='bg-white mr-3 shadow'>
       <Image source={{ uri: imgUrl }} className='h-36 w-64 rounded-sm' />
 
-      <View className='px-1 pb-4'>
+      <View className='px-3 pb-4'>
         <Text className='font-bold text-lg pt-2'>{title}</Text>
         <View className='flex-row items-center space-x-1'>
           <StarIcon size={22} color='green' opacity={0.5} />
